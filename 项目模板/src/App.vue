98cfe8ef142382c0de6c210ae3a641e994f612a8
lys <1097485836@qq.com>
2021-04-30 15:12:47 +0800

    lys

--- 项目模板/src/App.vue
+++ 项目模板/src/App.vue
@@ -1,4 +1,4 @@
-                                                                                                    <template>
+ <template>
   <div id="app">
     <router-view />
   </div>
@@ -14,31 +14,10 @@
 html {
   font-size: 50px;
 }
-<<<<<<< HEAD
+
 img{
   vertical-align: middle;
-<<<<<<< HEAD
-=======
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  color: #2c3e50;
->>>>>>> lys
-}
 
-#nav {
-  padding: 30px;
 
-  a {
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-=======
->>>>>>> humiaoran
 }
 </style>
