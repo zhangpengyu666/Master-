7525c3d0ba10ba840d10b2c9ee65e674077ffb36
ZXL102 <8899613+zxl102@user.noreply.gitee.com>
2021-05-05 19:36:23 +0800

    zxl

--- 项目模板/src/views/shisan.vue
+++ 项目模板/src/views/shisan.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class=''></div>
+    <div>
+       <img class="one" src="@/assets/image/images/切图/case/one.png" alt="">
+       <img class="two" src="@/assets/image/images/切图/case/two.png" alt="">
+       <img class="san" src="@/assets/image/images/切图/case/san.png" alt="">
+    </div>
 </template>
 
 <script>
@@ -13,5 +17,18 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-
+.one{
+    width: 100%;
+    height: 13rem;
+}
+.two{
+    width: 100%;
+    height: 13rem;
+    margin-top: 1rem;
+}
+.san{
+    width: 100%;
+    height: 20rem;
+    margin-top: 1rem;
+}
 </style>
